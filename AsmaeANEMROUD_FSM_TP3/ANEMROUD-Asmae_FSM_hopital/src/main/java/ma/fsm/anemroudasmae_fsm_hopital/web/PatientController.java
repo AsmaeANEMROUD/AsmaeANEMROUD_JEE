6704dd06228d839c0720c36b959cd43119ee87bc
AsmaeANEMROUD <164891923+AsmaeANEMROUD@users.noreply.github.com>
2024-04-20 05:55:54 +0100

    Add files via upload

---
+++ AsmaeANEMROUD_FSM_TP3/ANEMROUD-Asmae_FSM_hopital/src/main/java/ma/fsm/anemroudasmae_fsm_hopital/web/PatientController.java
@@ -0,0 +1,40 @@
+package ma.fsm.anemroudasmae_fsm_hopital.web;
+
+import lombok.AllArgsConstructor;
+import ma.fsm.anemroudasmae_fsm_hopital.entities.Patient;
+import ma.fsm.anemroudasmae_fsm_hopital.repositories.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class PatientController {
+    private PatientRepository patientRepository;
+
+    @GetMapping("/index")
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "0") int p,
+                        @RequestParam(name = "size", defaultValue = "4") int s,
+                        @RequestParam(name = "keyword", defaultValue = "") String kw){
+        Page<Patient> pagePatients = patientRepository.findByNomContains(kw, PageRequest.of(p,s));
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", p);
+        model.addAttribute("keyword", kw);
+        return "patients";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam(name ="id") Long id,
+                         @RequestParam(name ="keyword") String keyword,
+                         int page){
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+}
---
+++ AsmaeANEMROUD_FSM_TP3/ANEMROUD-Asmae_FSM_hopital/src/test/java/ma/fsm/anemroudasmae_fsm_hopital/AnemroudAsmaeFsmHopitalApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fsm.anemroudasmae_fsm_hopital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnemroudAsmaeFsmHopitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
