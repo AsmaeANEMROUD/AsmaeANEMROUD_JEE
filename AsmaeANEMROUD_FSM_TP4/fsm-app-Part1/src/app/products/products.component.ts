927c8fd9c24a797d6ac75fa86aa8ac3d361929d1
AsmaeANEMROUD <164891923+AsmaeANEMROUD@users.noreply.github.com>
2024-05-27 15:33:25 +0100

    Add files via upload

---
+++ AsmaeANEMROUD_FSM_TP4/fsm-app-Part1/src/app/products/products.component.ts
@@ -0,0 +1,62 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ProductService} from "../services/product.service";
+import {Product} from "../model/product.model";
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent implements OnInit{
+  public products : Array<Product> = [];
+  //products$! : Observable<Array<Product>>;
+  public keyword : string = "";
+  constructor(private productService : ProductService) {
+
+  }
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.productService.getProducts(1, 2)
+      .subscribe({
+        next : data => {
+          this.products = data
+        },
+        error : err => {
+          console.log(err);
+        }
+      })
+    //this.products$ = this.productService.getProducts();
+  }
+
+  handleCheckProduct(product : Product) {
+    this.productService.checkProduct(product).subscribe({
+      next : updatedProduct => {
+        product.checked = !product.checked;
+      }
+    })
+  }
+
+  handleDelete(product : Product) {
+    if (confirm("Etes vous sûre?"))
+    this.productService.deleteProduct(product).subscribe({
+      next : value => {
+        // this.getProducts();
+        this.products = this.products.filter(p => p.id != product.id);
+      }
+    })
+  }
+
+  searchProducts() {
+    this.productService.searchProducts(this.keyword).subscribe({
+      next : value => {
+        this.products = value;
+      }
+    })
+  }
+}
